12216a17f7268c17b478f69cc62a1c9f619e6862
gport <info@gport.nl>
2013-03-18 16:44:39 +0100

    Added comments in custom.js

--- custom.js
+++ custom.js
@@ -1,11 +1,12 @@
-// How to initialize the Paginated Quicksand 
-// Project: Paginated Quicksand
-// Version: [V0.1]
-// Last change: [18/4/2013] [Start of project]
+// Custom JS rules
+// Project: [Gport Framework]
+// Version: [V2.0]
+// Last change: [12/5/2011] [Start of project]
 // Assigned to: [Gerben van Dijk / Gport] [NL] 
 
 $(window).load(function() {
-		  
+	
+	// Initilazing jPages onload	  
 	$("div.pagination").jPages({
 	
 		containerID: "caseholder",
@@ -15,49 +16,71 @@ $(window).load(function() {
 	
 	});
 	  	
-	// if body has class animated
+	// Cloning the filterable element
 	var quicksand_data = $('.cases').clone();
-
+	
+	// Declaring the current category as global variable
+	var current_cat;
+	
+	// Firing the filter funtionality on click
 	$('.filter li a').on('click', function(e){
 
+		// Stop the page from jumping because of an empty href
 		e.preventDefault();
 		
-		//$("div.pagination").jPages("destroy");
-
+		// Getting the clicked category (data-cats attribute of the clicked link)
 		var filter_cat = $(this).data('cats');
 		
+		// If the clicked category is all, then show the original list
 		if(filter_cat == 'all'){ 
 		
 			filtered_data = quicksand_data.find('.item');
 						
-		} else {
+		} 
+		// If the clicked category is different to all, then filter the results
+		else {
 		
+			// Cloning the original elements
 			var data_new = quicksand_data.clone();
+			
+			// Removing all the elements that don't have the correct attribute
 			$(data_new).find('.item').not('[data-cats~="' + filter_cat + '"]').remove();
+			
+			// Finding all elements that have to be showed after filtering
 			filtered_data = $(data_new).find('.item');
 			
 		}
 		
-		$('.cases').quicksand(filtered_data, { 
+		// If the clicked category is different as the active one, then filter with QuickSand
+		if(current_cat != filter_cat) {
 			
-			duration: 700,
-			easing: 'jswing'
-						
-		}, function(){
+			$('.cases').quicksand(filtered_data, { 
+				
+				duration: 700,
+				easing: 'jswing'
+							
+			}, function(){
+	
+				// Overriding the injected styles from jPages
+				$('.cases').find('.item').css("display","block");
+				$('.cases').find('.item').css("opacity","1");			
+				
+				// Updating the pagination
+				$("div.pagination").jPages({
+				
+					containerID: "caseholder",
+					perPage: 4,
+					previous: "previous",
+					next: "next"		
+				
+				});			
+	
+			});
+			
+		}
 
-			$('.cases').find('.item').css("display","block");
-			$('.cases').find('.item').css("opacity","1");			
-			
-			$("div.pagination").jPages({
-			
-				containerID: "caseholder",
-				perPage: 4,
-				previous: "previous",
-				next: "next"		
-			
-			});			
-
-		});
+		// Setting the current category
+		current_cat = filter_cat;
 		
 		return false;
 				
